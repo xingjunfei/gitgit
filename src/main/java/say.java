482f0c07d2c2b0955162614ca752978b7da6631e
xing <xing@qq.com>
2023-01-09 20:50:48 +0800

    master change 6

--- src/main/java/say.java
+++ src/main/java/say.java
@@ -7,5 +7,6 @@ public class say {
         System.out.println("hot_fix做出了修改3");
         System.out.println("hot_fix做出了修改4");
         System.out.println("hot_fix做出了修改5");
+        System.out.println("hot_fix做出了修改6");
     }
 }
