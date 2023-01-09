d9529c9b13dd16a8618148ce10a6dd554cb8f3a7
xing <xing@qq.com>
2023-01-09 20:29:59 +0800

    hot_fix changes 1

--- src/main/java/say.java
+++ src/main/java/say.java
@@ -2,5 +2,6 @@ public class say {
     public static void main(String[] args) {
         System.out.println("hello ,git");
         System.out.println("222222");
+        System.out.println("hot_fix做出了修改1");
     }
 }
